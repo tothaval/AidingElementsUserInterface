500156f1d1d12d336eca51e9a8e5041f2589c009
fractal void <113776519+tothaval@users.noreply.github.com>
2023-11-29 15:54:05 +0100

    Add files via upload

--- Texts/TXT_0.cs
+++ Texts/TXT_0.cs
@@ -1,1 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace AidingElementsUserInterface.Texts
+{
+    internal class TXT_0
+    {
+        public string language { get; set; }
+
+        public TXT_0(string language)
+        {
+            this.language = language;
+        }
+
+        #region strings
+        public string quitQuestion()
+        {
+            if (language.Equals("english"))
+            {
+                return "Do you want to quit?";
+            }
+            else
+            {
+                return "language selection error";
+            }
+        }
+
+        public string quitTitle()
+        {
+            if (language.Equals("english"))
+            {
+                return "Aiding Elements UI shutdown";
+            }
+            else
+            {
+                return "language selection error";
+            }
+        }
+
+        public string shutdownQuestion()
+        {
+            if (language.Equals("english"))
+            {
+                return "Are you sure you want to shutdown your computer?";
+            }
+            else
+            {
+                return "language selection error";
+            }
+        }
+
+        public string shutdownTitle()
+        {
+            if (language.Equals("english"))
+            {
+                return "PC Shutdown";
+            }
+            else
+            {
+                return "language selection error";
+            }
+        }
+        #endregion strings
+
+    }
+}
