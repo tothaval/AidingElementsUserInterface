e716c480858f1c3e65936176973dfd196355d858
fractal void <113776519+tothaval@users.noreply.github.com>
2023-11-29 15:51:34 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+
+using Point = System.Windows.Point;
+using AidingElementsUserInterface.Core;
+using AidingElementsUserInterface.Texts;
+
+namespace AidingElementsUserInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool loaded = false;
+
+        internal CoreData coreData;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // processing
+        #region processing
+        private void load_borderDefaults()
+        {
+            border.Background = new SolidColorBrush(coreData.background);
+
+            border.BorderBrush = new SolidColorBrush(coreData.borderbrush);
+
+            border.CornerRadius = coreData.cornerRadius;
+
+            border.BorderThickness = coreData.thickness;
+
+        }
+
+
+        private void load_CoreCanvas()
+        {
+            canvas_border.Child = new CoreCanvas();
+        }
+
+        private void load_CoreData()
+        {
+            // if default_file not found
+            coreData = new CoreData();
+        }
+
+
+        public void quitAEUI()
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion processing
+
+
+
+
+        // events
+        #region events
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            quitAEUI();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            loaded = true;
+
+            int delayValue = 1;
+
+            await Task.Delay(delayValue);
+
+            load_CoreData();
+
+            load_borderDefaults();
+
+            load_CoreCanvas();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+
+            else if (e.ChangedButton == MouseButton.Right)
+            {
+
+            }
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Window_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+        }
+
+        #endregion events
+    }
+}
